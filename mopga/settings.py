fcaf9d13f2f11f8927b10527c9e898201f65258f
SALIM-Ussef <msalimyoussef@gmail.com>
2020-01-20 04:00:45 +0100

    implementation de Réinitialiser le mot de passe

--- mopga/settings.py
+++ mopga/settings.py
@@ -139,3 +139,11 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = "blog-home"
 
 LOGIN_URL = "login"
+
+# L'envoi des e-mail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'msalimyoussef@gmail.com'
+EMAIL_HOST_PASSWORD = 'psyp tcus wsfg ufhi'
--- users/views.py
+++ users/views.py
@@ -3,17 +3,19 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
+
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            #username = form.cleaned_data.get("username")
+            # username = form.cleaned_data.get("username")
             form.save()
             messages.success(request, "Votre compte a été crée avec succès. Vous pouvez vous connecter !")
             return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
 
 @login_required
 def profile(request):
