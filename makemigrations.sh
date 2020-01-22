84a8994f7143c21d360bf4b8e0bffa8b4efd8952
dndayishima <divin.ndayishima@outlook.com>
2020-01-22 13:15:26 +0100

    Update script makemigrations.sh

--- makemigrations.sh
+++ makemigrations.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd /app/mopga_django
+
+# changement de branche
+git checkout develop-dndayishima
+
+# exécution des migrations
 python manage.py makemigrations main users
 python manage.py migrate
